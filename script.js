cb62f25866cdf145476535e214b2970125c6658b
bmelta2022 <bilalmahmood99@hotmail.com>
2024-11-10 14:20:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function showSlides() {
 
 function plusSlides(n) {
     slideIndex += n;
+    let slides = document.getElementsByClassName("slide");
     if (slideIndex > slides.length) { slideIndex = 1; }
     if (slideIndex < 1) { slideIndex = slides.length; }
-    showSlides();
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
