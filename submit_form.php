ed1c7775515200bf4996495dc910659eaf5b874a
bmelta2022 <bilalmahmood99@hotmail.com>
2024-11-10 16:11:39 -0500

    Update

---
+++ submit_form.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $phone = htmlspecialchars(trim($_POST['phone']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
+    $to = "Noman.mahmood1@hotmail.com";
+    $subject = "New Contact Form Submission from " . $name;
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n";
+    $body .= "Message:\n$message\n";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Oops! Something went wrong, please try again later.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
