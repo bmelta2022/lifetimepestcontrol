5b072f6b9c253a1fa4f7ab282998764bee22d331
bmelta2022 <bilalmahmood99@hotmail.com>
2024-11-12 20:08:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ function plusSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
 }
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+function toggleAccordion(element) {
+    const content = element.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
